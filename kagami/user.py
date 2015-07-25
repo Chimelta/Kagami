c7921dc3d1d962d51bcd02565adfb1dc42f376fe
LackOfDream <galaxy0419@gmail.com>
2015-07-26 00:36:25 +0900

    user function

--- kagami/user.py
+++ kagami/user.py
@@ -12,11 +12,14 @@ def user(username: str):
     page = '1'
     if request.args.get('page', ''):
         page = request.args.get('page', '')
+    friendships = None
+    if username != session.get('me'):
+        friendships = api.show_friendship(source_screen_name=session.get('me'), target_screen_name=username)[0]
     status_list = api.user_timeline(username, page=int(page))
     user = api.get_user(username)
     return render_template('user.html', head='@'+username+' ', statuses=status_list, me=session.get('me'),
                            page=page, npage=int(page)+1, ppage=int(page)-1, username=username,
-                           handler=kagami.handler, user=user)
+                           handler=kagami.handler, user=user, friendship=friendships)
 
 
 @app.route('/me')
@@ -25,4 +28,17 @@ def me():
         abort(401)
     api = get_api(app.config['CK'], app.config['CS'], session.get('at'), session.get('as'))
     username = api.me().screen_name
+    return redirect(url_for('user', username=username))
+
+
+@app.route('/follow/<username>')
+def follow(username: str):
+    api = get_api(app.config['CK'], app.config['CS'], session.get('at'), session.get('as'))
+    api.create_friendship(screen_name=username)
+    return redirect(url_for('user', username=username))
+
+@app.route('/unfollow/<username>')
+def unfollow(username: str):
+    api = get_api(app.config['CK'], app.config['CS'], session.get('at'), session.get('as'))
+    api.destroy_friendship(screen_name=username)
     return redirect(url_for('user', username=username))
